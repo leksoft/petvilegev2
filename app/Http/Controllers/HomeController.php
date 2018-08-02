92ab550de07fa7e2781aa7584c33a1389b13fa0f
leksoft <leksoft@rmu.ac.th>
2018-08-02 16:34:14 +0700

    สร้างดีล

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
+
     public function __construct()
     {
        // $this->middleware('auth');
@@ -36,6 +38,7 @@ class HomeController extends Controller
 
         } elseif (Auth::user()->role_id == 3) { // 3 = partner
             return view('backoffice.partner');
+            //backoffice
 
         }
 
